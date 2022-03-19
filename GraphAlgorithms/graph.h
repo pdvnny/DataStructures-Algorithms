ae9d2399206ea22d645a088853029b76c9351d23
dunny <pdunn91@gmail.com>
2022-03-19 01:35:11 -0400

    Got a graph structure working!
    NEXT UP: figure out how to implement DFS/BFS with the graph structure that I designed.

--- GraphAlgorithms/graph.h
+++ GraphAlgorithms/graph.h
@@ -26,33 +26,64 @@
 
 using namespace std;
 
+template <class k>
+class Edge {
+public:
+    k start;
+    k end;
+    int weight;
+};
+
+template <class k>
+class Node {
+public:
+    k key;
+    vector<Edge<k>> edges;
+};
+
+
 template <class v>
-class Graph {
+class DirectedGraph {
 public:
     
     int vertices;
     int edges;
-    vector<v> V;
-    vector<vector<v>> E;
+    vector<Node<v>> V;
+    //vector<Edge<v>> E;
 
-    Graph(v verts[], vector<v> edges[], int n_vert, int n_edges) {
+    DirectedGraph(v verts[], Edge<char> edgs[], int n_vert, int n_edges) {
         this->vertices = n_vert;
         this->edges = n_edges;
 
 
         /// Copying over vertices
-        for (int i = 0; i < vertices; i++) V.push_back(verts[i]);
+        for (int i = 0; i < vertices; i++) { //V.push_back(verts[i]);
+            Node<v> tmp_node;
+            tmp_node.key = verts[i];
+            V.push_back(tmp_node);
+        }
 
         /// Copying the edges
-        for (auto lst : E) E.push_back(lst);  /// <-- should be saving an entire vector that represents and adjancency list each time
+        //for (auto lst : E) E.push_back(lst);  /// <-- should be saving an entire vector that represents and adjancency list each time
+        for (int i = 0; i < edges; i++) {
+            v src = edgs[i].start;
+
+            /// find src node in list of vertices
+            int j = 0;
+            while (V[j].key != src) j++;
+
+            /// save edge to the node
+            V[j].edges.push_back(edgs[i]);
+        }
     }
 
-    ~Graph() {}
+    ~DirectedGraph() {}
 
     void print() {
         for (int i = 0; i < vertices; i++) {
-            cout << "Vertex " << V.at(i) << ": ";
-            for (auto edg : E.at(i)) cout << edg << ", ";
+            Node n = V.at(i);
+            cout << "Vertex " << n.key << ": ";
+            for (auto edge : V.at(i).edges) cout << edge.end << ", ";
             cout << endl;
         }
     }
